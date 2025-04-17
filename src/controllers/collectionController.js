c7f848d53a817f7d06ac0e1a29995f6f7c27173d
anna <acfiochi@gmail.com>
2025-04-17 11:42:01 -0300

    feat: create post method

--- src/controllers/collectionController.js
+++ src/controllers/collectionController.js
@@ -65,42 +65,31 @@ class CollectionController {
     }
   }
 
-  // PUT /api/personagens/:id
-  async updatePersonagem(req, res) {
+  // PUT /colecoes/:id
+  async updateCollection(req, res) {
     try {
       const { id } = req.params;
       const {
-        title,
+        name,
         description,
-        episodes,
         releaseYear,
-        studio,
-        genres,
-        rating,
-        imageUrl,
       } = req.body;
 
       // Atualizar o personagem
-      const updatedPersonagem = await PersonagemModel.update(
-        id,
-        title,
+      const updatedCollection = await CollectionModel.update(
+        name,
         description,
-        episodes,
         releaseYear,
-        studio,
-        genres,
-        rating,
-        imageUrl
       );
 
-      if (!updatedPersonagem) {
-        return res.status(404).json({ error: "Personagem não encontrado" });
+      if (!updatedCollection) {
+        return res.status(404).json({ error: "colecao não encontrada" });
       }
 
-      res.json(updatedPersonagem);
+      res.json(updatedCollection);
     } catch (error) {
-      console.error("Erro ao atualizar personagem:", error);
-      res.status(500).json({ error: "Erro ao atualizar personagem" });
+      console.error("Erro ao atualizar colecao:", error);
+      res.status(500).json({ error: "Erro ao atualizar colecao" });
     }
   }
 
